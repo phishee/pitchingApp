d0b69bc73878c879fde110e0bd710d66de619eca
saxanws <axansimilien@gmail.com>
2025-08-23 23:04:11 -0600

    vercel-2.3

--- src/app/api/v1/team-members/[id]/route.ts
+++ src/app/api/v1/team-members/[id]/route.ts
@@ -5,17 +5,17 @@ import container from "@/app/api/lib/container";
 import { TEAM_MEMBER_TYPES } from "@/app/api/lib/symbols/Symbols";
 import { TeamMemberController } from "@/app/api/lib/controllers/team-member.controller";
 
-export async function GET(req: NextRequest, params: { params: Promise<{ id: string }> }) {
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
     const teamMemberController = container.get<TeamMemberController>(TEAM_MEMBER_TYPES.TeamMemberController);
-    return teamMemberController.getTeamMemberById(req, params);
+    return teamMemberController.getTeamMemberById(req, context);
 }
 
-export async function PUT(req: NextRequest, params: { params: Promise<{ id: string }> }) {
+export async function PUT(req: NextRequest, context: { params: Promise<{ id: string }> }) {
     const teamMemberController = container.get<TeamMemberController>(TEAM_MEMBER_TYPES.TeamMemberController);
-    return teamMemberController.updateTeamMember(req, params);
+    return teamMemberController.updateTeamMember(req, context);
 }
 
-export async function DELETE(req: NextRequest, params: { params: Promise<{ id: string }> }) {
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
     const teamMemberController = container.get<TeamMemberController>(TEAM_MEMBER_TYPES.TeamMemberController);
-    return teamMemberController.deleteTeamMember(req, params);
+    return teamMemberController.deleteTeamMember(req, context);
 }
